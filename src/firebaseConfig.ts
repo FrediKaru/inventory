f4f1dd2157eff8dfbf49fa3432ab8c8f96b2c95f
Fredi Karu <fredikaru@gmail.com>
2024-08-11 21:15:54 +0300

    Install firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIz1iVzwRg3VIUnO_WyIBUEW1N3RuK5Is",
+  authDomain: "inventory-50c74.firebaseapp.com",
+  projectId: "inventory-50c74",
+  storageBucket: "inventory-50c74.appspot.com",
+  messagingSenderId: "466060290039",
+  appId: "1:466060290039:web:6724905a0c8c9ceaa430f2",
+  measurementId: "G-ERZF63MXYZ",
+};
