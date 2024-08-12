a62ab5eab16f83a124a89639462f2ff811b61072
Fredi Karu <fredikaru@gmail.com>
2024-08-13 01:17:39 +0300

    Style active link in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,24 +1,57 @@
 import React from "react";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Sidebar() {
+  const location = useLocation();
+
   return (
-    <div className="sidebar">
+    <div className="sidebar bg-primary">
       <div>
         <h2 className="component-label">Lighttrack</h2>
-        <ul>
+        <ul
+          className="flex
+         flex-col gap-3"
+        >
           <li>
-            <NavLink to="/inventory">Dashboard</NavLink>
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) =>
+                isActive ? "active-button sidebar-button" : "sidebar-button"
+              }
+            >
+              Dashboard
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/inventory">Inventory</NavLink>
+            <NavLink
+              to="/inventory"
+              className={({ isActive }) =>
+                isActive ? "active-button sidebar-button" : "sidebar-button"
+              }
+            >
+              Inventory
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/inventory">Reservations</NavLink>
+            <NavLink
+              to="/reservations"
+              className={({ isActive }) =>
+                isActive ? "active-button sidebar-button" : "sidebar-button"
+              }
+            >
+              Reservations
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/preferences">Preferences</NavLink>
+            <NavLink
+              to="/preferences"
+              className={({ isActive }) =>
+                isActive ? "active-button sidebar-button" : "sidebar-button"
+              }
+            >
+              Preferences
+            </NavLink>
           </li>
         </ul>
       </div>
