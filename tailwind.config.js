5278cd72e66be5d0b5593bba32e82759f1758e87
Fredi Karu <fredikaru@gmail.com>
2024-08-13 01:16:49 +0300

    Add color palette into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      lightPrimary: "#F2F8F6",
+      primary: "#001D22",
+    },
   },
   plugins: [],
 };
