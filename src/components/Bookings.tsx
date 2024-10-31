ffb1561d34270e8f54874e6a7b6c7cceb729b768
Fredi Karu <fredikaru@gmail.com>
2024-10-31 02:30:21 +0200

    Extract bookings component into separate file

---
+++ src/components/Bookings.tsx
@@ -0,0 +1,56 @@
+import { BookingProps } from "../routes/Inventory";
+import { NavLink } from "react-router-dom";
+// Define props for BookingsMonitor
+
+type BookingsProps = {
+  bookings: BookingProps[];
+};
+
+const Bookings = ({ bookings }: BookingsProps) => (
+  <section className="my-10 dashboard-section">
+    <h2>Recent bookings</h2>
+    <div>
+      <table className="bookings-table w-full my-6">
+        <thead className="uppercase text-xs font-medium text-Primary">
+          <tr className="text-left">
+            <th>Name</th>
+            <th>Title</th>
+            <th>Items</th>
+            <th>Starting date</th>
+            <th>Ending date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookings.map((booking) => (
+            <tr key={booking.id} className="result">
+              <td>{booking.name}</td>
+              <td>{booking.title}</td>
+              <td>12</td>
+              <td>
+                {booking.startingDate
+                  ? new Date(booking.startingDate).toLocaleDateString()
+                  : ""}
+              </td>
+              <td>
+                {booking.endingDate
+                  ? new Date(booking.endingDate).toLocaleDateString()
+                  : ""}
+              </td>
+              <td className="text-lightPrimary">
+                <NavLink
+                  to={`/b/${booking.id}`}
+                  className={"px-6 py-2 rounded-sm bg-primary"}
+                >
+                  Edit
+                </NavLink>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </section>
+);
+
+export default Bookings;
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { getProductsCount, getBookings } from "../inventoryItems";
 import { BookingProps } from "./Inventory";
+import { getProductsCount, getBookings } from "../inventoryItems";
+
+import Bookings from "../components/Bookings";
 
 type OverviewCardsProps = {
   totalItems: number;
@@ -37,43 +39,6 @@ const QuickActions = () => (
       <button>Add New Item</button>
       <button>Check In/Out</button>
       <button>Generate Report</button>
-    </div>
-  </section>
-);
-
-// Define props for BookingsMonitor
-type BookingsMonitorProps = {
-  bookings: BookingProps[];
-};
-
-const BookingsMonitor = ({ bookings }: BookingsMonitorProps) => (
-  <section className="my-10 dashboard-section">
-    <h2>Recent bookings</h2>
-    <div>
-      <table className="bookings-table w-full my-6">
-        <thead className="uppercase text-xs font-bold text-Primary">
-          <td>Name</td>
-          <td>Title</td>
-          <td>Items</td>
-          <td>Starting date</td>
-          <td>Ending date</td>
-          <td>Actions</td>
-        </thead>
-        <tbody>
-          {bookings.map((booking) => (
-            <tr key={booking.id} className="result">
-              <td>{booking.name}</td>
-              <td>{booking.title}</td>
-              <td>12</td>
-              <td>{booking.startingDate.toLocaleString().split("T")[0]}</td>
-              <td>{booking.endingDate.toLocaleString().split("T")[0]}</td>
-              <td>
-                <NavLink to={`/b/${booking.id}`}>Edit</NavLink>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   </section>
 );
@@ -120,7 +85,7 @@ function Dashboard() {
           totalBookings={bookings.length || 99}
         />
         <QuickActions />
-        <BookingsMonitor bookings={bookings} />
+        <Bookings bookings={bookings} />
         <Statistics />
       </main>
     </div>
