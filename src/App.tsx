af8f785fcba9bfc32a22c0db8130e9dd6415d0b0
Fredi Karu <fredikaru@gmail.com>
2024-11-07 02:04:28 +0200

    Fix typescript errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
