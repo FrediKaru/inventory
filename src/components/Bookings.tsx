8f42881c00bd31884eef72468300928fc7134345
Fredi Karu <fredikaru@gmail.com>
2024-11-11 00:54:27 +0200

    Fetch bookings with react-query

--- src/components/Bookings.tsx
+++ src/components/Bookings.tsx
@@ -1,4 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import { getBookings } from "../inventoryItems";
 
 export interface BookingProps {
   id: string;
@@ -10,58 +12,61 @@ export interface BookingProps {
 }
 
 export type BookingsProps = {
-  bookings: BookingProps[];
   handleDelete: (id: string) => void;
 };
 
-const Bookings = ({ bookings, handleDelete }: BookingsProps) => (
-  <section className="my-10 dashboard-section">
-    <h2>Recent bookings</h2>
-    <div>
-      <table className="bookings-table w-full my-6">
-        <thead className="uppercase text-xs font-medium text-Primary">
-          <tr className="text-left">
-            <th>Name</th>
-            <th>Title</th>
-            <th>Items</th>
-            <th>Starting date</th>
-            <th>Ending date</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {bookings.map((booking) => (
-            <tr key={booking.id} className="result">
-              <td>{booking.name}</td>
-              <td>{booking.title}</td>
-              <td>{booking.savedItems.length}</td>
-              <td>
-                {booking.startingDate
-                  ? new Date(booking.startingDate).toLocaleDateString()
-                  : ""}
-              </td>
-              <td>
-                {booking.endingDate
-                  ? new Date(booking.endingDate).toLocaleDateString()
-                  : ""}
-              </td>
-              <td className="flex btn-group gap-2">
-                <Link to={`/b/${booking.id}`} className="btn">
-                  Edit
-                </Link>
-                <button
-                  className="btn"
-                  onClick={() => handleDelete(booking.id)}
-                >
-                  Delete
-                </button>
-              </td>
+function Bookings({ handleDelete }: BookingsProps) {
+  const query = useQuery({ queryKey: ["bookings"], queryFn: getBookings });
+
+  return (
+    <section className="my-10 dashboard-section">
+      <h2>Recent bookings</h2>
+      <div>
+        <table className="bookings-table w-full my-6">
+          <thead className="uppercase text-xs font-medium text-Primary">
+            <tr className="text-left">
+              <th>Name</th>
+              <th>Title</th>
+              <th>Items</th>
+              <th>Starting date</th>
+              <th>Ending date</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </section>
-);
+          </thead>
+          <tbody>
+            {query.data?.map((booking) => (
+              <tr key={booking.id} className="result">
+                <td>{booking.name}</td>
+                <td>{booking.title}</td>
+                <td>{booking.savedItems.length}</td>
+                <td>
+                  {booking.startingDate
+                    ? new Date(booking.startingDate).toLocaleDateString()
+                    : ""}
+                </td>
+                <td>
+                  {booking.endingDate
+                    ? new Date(booking.endingDate).toLocaleDateString()
+                    : ""}
+                </td>
+                <td className="flex btn-group gap-2">
+                  <Link to={`/b/${booking.id}`} className="btn">
+                    Edit
+                  </Link>
+                  <button
+                    className="btn"
+                    onClick={() => handleDelete(booking.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
 
 export default Bookings;
